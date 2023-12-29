f6c2f4b4a7a2384fc0f1fd97a6c21a3958c20834
gaurav_1807 <gauravambaliya77@gmail.com>
2023-12-29 12:40:51 +0530

    new

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connect =async()=>{
-    await mongoose.connect(`mongodb://127.0.0.1:27017/finalproject`)
+    await mongoose.connect(`mongodb+srv://gauravambaliya77:gaurav@cluster0.6hcuzhv.mongodb.net/`)
     console.log("Connect to MongoDB")
 }
 
